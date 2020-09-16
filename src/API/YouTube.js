e3060e7c87b9d1a1c316d40c87fa7e5c054f3950
sukh <sukhveer.sangha@tatacommunications.com>
2020-09-16 15:24:27 +0100

    axios set up with baseURL

--- src/API/YouTube.js
+++ src/API/YouTube.js
@@ -1,2 +1,10 @@
 import axios from 'axios'
 
+export default axios.create({
+    baseURL: 'https://googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 10,
+        key: 'AIzaSyAgT7fUni1dYQ6arrUgcFIx7sP761w7GjU'
+    }
+})
