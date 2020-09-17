c0e7df993d100dc988aded04a457046a6d81f351
sukh <sukhveer.sangha@tatacommunications.com>
2020-09-17 11:16:25 +0100

    built out the searchbar component

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,9 +1,37 @@
 import React from 'react'
 
+import { Paper, TextField } from '@material-ui/core'
+
 class SearchBar extends React.Component {
+    state = {
+        searchTerm: '',
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            searchTerm: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        // prevent default browser refresh
+        e.preventDefault()
+
+        // destructure the search term - to avoid repetition of multiple search terms
+        const { searchTerm } = this.state
+        const { onFormSubmit } = this.props
+
+        onFormSubmit(searchTerm)
+        
+    }
+
     render(){
         return (
-            <h1>searchbar component</hi>
+           <Paper elevation={6} style={{ padding: '25px' }}>
+            <form onSubmit={this.handleSubmit}>
+                <TextField fullwidth label="Search..." onChange={this.handleChange} />
+            </form>
+           </Paper>
         )
     }
 }
