905efe3525ebf99544a8d5ddc90e71ce48e33913
sukh <sukhveer.sangha@tatacommunications.com>
2020-09-17 09:35:33 +0100

    set up app.js structure and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
+import { Grid } from '@material-ui/core'
+
+import { SearchBar, VideoList, VideoDetail } from './Components'
+
+import youtube from './API/YouTube'
+// reference to axios instance
+
 class App extends React.Component {
     render(){
         return(
-            <h1>YouTube Clone App</h1>
+            <Grid justify="centre" container spacing={16}>
+                <Grid item xs={12}>
+                    <Grid container spacing={16}>
+                        <Grid item xs={12}>
+                            {/** Search Bar */}
+                        </Grid>
+                        <Grid item xs={8}>
+                            {/** Video Details */}
+                        </Grid>
+                        <Grid item xs={4}>
+                            {/** Video List */}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
         )
     }
 }
